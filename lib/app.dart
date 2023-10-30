00d5b77a440c82fea1d97e88b200ca8bc88aac74
Khayyam Ahmed <ahmed.khayyam012@gmail.com>
2023-10-30 11:10:05 +0500

    added singout logic, Splash Sreen, StreamBuilder to display different screens based on user credentials

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,13 @@
 import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:chat_app/screens/splash_screen.dart';
 import 'package:chat_app/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/utils/themes/theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +15,14 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
-      home: const AuthScreen(),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
+            return snapshot.hasData ? const ChatScreen() : const AuthScreen();
+          }),
     );
   }
 }
